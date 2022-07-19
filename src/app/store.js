eb953556a3084e712dbdf278effc288a2cccd591
tabburi <tharun_abburi@mcafee.com>
2022-07-19 12:45:30 +0530

    feat: client UI screen

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import commonState from "../feature/commonState";
-import customComponent from "../feature/customComponent";
 
 export default configureStore({
   reducer: {
     counter: commonState,
-    customComponent: customComponent,
   },
 });
