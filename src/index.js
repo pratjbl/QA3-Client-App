2e946cfe28eb21c7d9d9cd7e8a9d9ff81c37221a
tabburi <tharun_abburi@mcafee.com>
2023-01-20 16:08:39 +0530

    fix:scope change

--- src/index.js
+++ src/index.js
@@ -24,7 +24,8 @@ const providerConfig = {
   clientId: config.clientId,
   ...(config.audience ? { audience: config.audience } : null),
   redirectUri: window.location.origin,
-  scope: "openid profile email ",
+  scope:
+    "openid profile email enroll read:authenticators remove:authenticators",
   onRedirectCallback,
 };
 
